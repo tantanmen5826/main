90e3402179b55c100f44e87ed88cde583aa320af
AKIMOTOYUJI5826\akkue <akkueria@gmail.com>
2025-06-03 11:15:24 +0900

    テスト

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace Todo.Controllers
             return View();
         }
 
+        /// <summary>
+        /// ƒeƒXƒg
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Privacy()
         {
             return View();
